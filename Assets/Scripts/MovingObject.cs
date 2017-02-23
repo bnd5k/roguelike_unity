9016676bc0ae87e81fd5db2a1e400b207db5bd80
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-02-23 11:30:46 -0800

    Adds MovingObject.  Vid 6.

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+	public float moveTime = 0.1f;
+	public LayerMask blockingLayer;
+
+
+	private BoxCollider2D boxCollider;
+	private Rigidbody2D rb2D;
+	private float inverseMoveTime;
+
+
+	protected virtual void Start () {
+		// 	protected virtual  means that the function can be overridden by its child classes
+		boxCollider = GetComponent<BoxCollider2D>();
+		rb2D = GetComponent<Rigidbody2D> ();
+		inverseMoveTime = 1f / moveTime;
+	}
+
+	protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
+	{
+		// out mean that we'll pass this by reference
+		Vector2 start = transform.position;
+		Vector2 end = start + new Vector2 (xDir, yDir);
+
+		boxCollider.enabled = false;
+		hit = Physics2D (start, end, blockingLayer);
+		boxCollider.enabled = true;
+
+		if (hit.transform == null) {
+			StartCoroutine (SmoothMovement (end));
+			return true;
+		}
+		return false
+
+	}
+
+	protected IEnumerator SmoothMovement(Vector3 end)
+	{
+		float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+		while (sqrRemainingDistance > float.Epsilon) {
+			Vector3 newPosition = Vector3 (rb2D.position, end, inverseMoveTime * Time.deltaTime);
+			rb2D.MovePosition (newPosition);
+			sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+			yield return null
+		}
+	}
+
+	protected virtual void AttemptMove <T> (int Xdir, int yDir)
+
+	protected abstract void onCantMove <T> (T Component)
+		where T : Component;
+	// T is a generic paramter.  Short for "type"
+	// we're saying this is impcomplete and that the child objects will flesh it out.
+	// note that we don't need brackets
+
+}
