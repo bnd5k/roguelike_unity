4a65c4ae8fc29ef964df0e3b7255e1afe6d58463
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-02-23 12:06:07 -0800

    Add wall. Vid 7

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -27,31 +27,44 @@ public abstract class MovingObject : MonoBehaviour {
 		Vector2 end = start + new Vector2 (xDir, yDir);
 
 		boxCollider.enabled = false;
-		hit = Physics2D (start, end, blockingLayer);
+		hit = Physics2D.Linecast (start, end, blockingLayer);
 		boxCollider.enabled = true;
 
 		if (hit.transform == null) {
 			StartCoroutine (SmoothMovement (end));
 			return true;
 		}
-		return false
-
+		return false;
 	}
 
 	protected IEnumerator SmoothMovement(Vector3 end)
 	{
 		float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
 		while (sqrRemainingDistance > float.Epsilon) {
-			Vector3 newPosition = Vector3 (rb2D.position, end, inverseMoveTime * Time.deltaTime);
+			Vector3 newPosition = Vector3.MoveTowards (rb2D.position, end, inverseMoveTime * Time.deltaTime);
 			rb2D.MovePosition (newPosition);
 			sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-			yield return null
+			yield return null;
 		}
 	}
 
-	protected virtual void AttemptMove <T> (int Xdir, int yDir)
+	protected virtual void AttemptMove <T> (int xDir, int yDir)
+		where T: Component
+	{
+		RaycastHit2D hit;
 
-	protected abstract void onCantMove <T> (T Component)
+		bool canMove = Move (xDir, yDir, out hit);
+		if (hit.transform == null) {
+			return;
+		}
+		T hitComponent = hit.transform.GetComponent<T>();
+
+		if (!canMove && hitComponent != null) {
+			OnCantMove(hitComponent);
+		}
+	}
+
+	protected abstract void OnCantMove <T> (T Component)
 		where T : Component;
 	// T is a generic paramter.  Short for "type"
 	// we're saying this is impcomplete and that the child objects will flesh it out.
