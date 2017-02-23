f3def1b8963e9a9fdb92ad92b5c5cd24ded5bd66
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-02-23 09:03:46 -0800

    Adds GameManager. Vid 5.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-	public BoardManager boardScript;
 
+	public static GameManager instance = null;
+
+	public BoardManager boardScript;
 
 	private int level = 3;
 
-
-
 	void Awake () {
+		if (instance == null) { 
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+
 		boardScript = GetComponent<BoardManager> ();
 		InitGame ();
 	}
