435bf9b20ba30459cc91902f9652d9fbd9dacd2e
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-02-24 14:02:01 -0800

    Adds game play placards. Vid 12

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+	public float levelStartDelay = 2f;
 	public float turnDelay = .1f;
 	public static GameManager instance = null;
 
@@ -11,9 +14,12 @@ public class GameManager : MonoBehaviour {
 	public int playerFoodPoints = 100;
 	[HideInInspector] public bool playersTurn = true;
 
-	private int level = 3;
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 1;
 	private List<Enemy> enemies;
 	private bool enemiesMoving;
+	private bool doingSetup;
 
 	void Awake () {
 		if (instance == null) { 
@@ -30,18 +36,55 @@ public class GameManager : MonoBehaviour {
 		InitGame ();
 	}
 
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode
+		mode)
+	{
+		//Add one to our level number.
+		level++;
+		//Call InitGame to initialize our level.
+		InitGame();
+	}
+
+	void OnEnable()
+	{
+		//Tell our ‘OnLevelFinishedLoading’ function to start listening for a scene change event as soon as this script is enabled.
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+
+	void OnDisable()
+	{
+		//Tell our ‘OnLevelFinishedLoading’ function to stop listening for a scene change event as soon as this script is disabled.
+		//Remember to always have an unsubscription for everydelegate you subscribe to!
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
+
 	void InitGame()
 	{
+		doingSetup = true;
+
+		levelImage = GameObject.Find ("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text> ();
+		levelText.text = "Day " + 1;
+		levelImage.SetActive (true);
+		Invoke ("HideLevelImage", levelStartDelay);
+
 		boardScript.SetupScene (level);
 		enemies.Clear ();
 	}
 
+	private void HideLevelImage()
+	{
+		levelImage.SetActive(false);
+		doingSetup = false;
+	}
+
 	public void GameOver() {
+		levelText.text = "After " + level + " days, you starved";
 		enabled = false;
 	}
 		
 	void Update () {
-		if (playersTurn || enemiesMoving) {
+		if (playersTurn || enemiesMoving || doingSetup) {
 			return;
 		}
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MovingObject {
 
@@ -9,6 +10,7 @@ public class Player : MovingObject {
 	public int pointsPerFood = 10;
 	public int pointsPerSoda = 20;
 	public float restartLevelDelay = 1f;
+	public Text foodText;
 
 	private Animator animator;
 
@@ -20,6 +22,7 @@ public class Player : MovingObject {
 		animator = GetComponent<Animator> ();
 		food = GameManager.instance.playerFoodPoints;
 
+		foodText.text = "Food: " + food;
 		base.Start();
 	}
 	
@@ -49,6 +52,7 @@ public class Player : MovingObject {
 	protected override void AttemptMove <T> (int xDir, int yDir)
 	{
 		food--;
+		foodText.text = "Food: " + food;
 
 		base.AttemptMove <T> (xDir, yDir);
 
@@ -65,9 +69,11 @@ public class Player : MovingObject {
 			enabled = false;
 		} else if (other.tag == "Food") {
 			food += pointsPerFood;
+			foodText.text = "+" + pointsPerFood + " Food: " + food;
 			other.gameObject.SetActive (false);
 		} else if (other.tag == "Soda") {
 			food += pointsPerSoda;
+			foodText.text = "+" + pointsPerSoda + " Food: " + food;
 			other.gameObject.SetActive(false);
 		}
 	}
@@ -87,6 +93,7 @@ public class Player : MovingObject {
 	public void LoseFood(int loss) {
 		animator.SetTrigger ("playerHit");
 		food -= loss;
+		foodText.text = "-" + loss + " Food: " + food;
 		CheckIfGameOver ();
 	}
 
